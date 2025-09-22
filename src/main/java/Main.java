02c17bf662137699132a4ac810bb0577f877c2ec
Stefanvill <stefan@villegas.se>
2025-09-22 17:09:20 +0200

    bug fixes!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
-import java.sql.SQLException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Meny m = new Meny();
