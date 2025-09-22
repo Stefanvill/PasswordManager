0fd5bf7712e2f4e7f49a8a65122926c2a7e3edbd
Stefanvill <stefan@villegas.se>
2025-09-22 17:07:46 +0200

    Updates for cleaner code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,49 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseManager dbManager = new DatabaseManager();
-        dbManager.connect();
+        Meny m = new Meny();
+        m.menyRun();
 
-        UserManager userManager = new UserManager(dbManager);
-        Scanner sc = new Scanner(System.in);
 
-        while (true) {
-
-            try {
-
-                System.out.println("Choose and option\n1. Add credentials\n2. Show credentials by URL\n3. Cancel\n");
-                int userInput = sc.nextInt();
-                if (userInput == 1) {
-                    System.out.println("Write url: ");
-                    String uUrl = sc.next();
-                    System.out.println("Write username: ");
-                    String uUser = sc.next();
-                    System.out.println("Write password: ");
-                    String uPass = sc.next();
-
-                    boolean inserted = userManager.addCredentials(uUrl, uUser, uPass);
-
-                } else if (userInput == 2) {
-                    System.out.println("Please write a url: ");
-                    String uUrl = sc.next();
-                    userManager.printCredentialsbyUrl(uUrl);
-
-                } else if (userInput == 3) {
-                    System.out.println("You choose to exit!");
-                    dbManager.disconnect();
-                    sc.close();
-                    break;
-                } else {
-                    System.out.println("Please put in a integer between 1-3");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Please put in a integer not a String");
-                sc.nextLine();
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
     }
 
 }
